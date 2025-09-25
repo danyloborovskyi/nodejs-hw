eb5f41963860da5a60661b3a5292d340a266da97
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-09-26 00:23:05 +0300

    fixed error

--- src/server.js
+++ src/server.js
@@ -40,12 +40,12 @@ app.get('/notes/:noteId', (req, res) => {
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
 });
 
 app.use((err, req, res, next) => {
